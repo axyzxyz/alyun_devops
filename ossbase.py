2d8992a68200dc9ee6f9154d81061053f9edb805
yuesj <yuesj@kmelearning.com>
2018-11-21 11:12:39 +0800

    oss 上传小文件

--- ossbase.py
+++ ossbase.py
@@ -6,6 +6,7 @@ import os
 import logging
 import oss2
 
+
 logging.basicConfig(level=logging.WARNING,
                     format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 
@@ -18,6 +19,8 @@ class Oss:
         if not endpoint:
             logging.warning("endpoint is not set ,will used 'http://oss-cn-hangzhou.aliyuncs.com'\n")
             self.endpoint = 'http://oss-cn-hangzhou.aliyuncs.com'
+        else:
+            self.endpoint = endpoint
         self.bucket = oss2.Bucket(self.auth, self.endpoint, bucket)
 
     def bucket_change(self, bucket, endpoint=None):
@@ -58,10 +61,39 @@ class Oss:
             else:
                 os.rename(root_dir, ldir)
 
+    def push_file(self, filename, ossfilename=None, cdn_host=None):
+        """
+
+        :param filename: 本地文件地址
+        :param ossfilename: oss存储文件名，为空则为filename
+        :cdn_host cdn的域名，带http or https，不存在则为公网oss地址
+        :return:链接地址
+
+        """
+        if not os.path.isfile(filename):
+            print("warnning: the {} is not a file or not existed".format(filename))
+            return
+        base_name = os.path.basename(filename)
+        ossfilename = ossfilename or base_name
+        result = self.bucket.put_object_from_file(ossfilename, filename)
+        url = result.resp.response.url
+        internal_url = url.replace('.aliyuncs.com/', '-internal.aliyuncs.com/')
+        if cdn_host:
+            if cdn_host.lower().startswith(['https', 'http']):
+                cdn_url = cdn_host.strip() + result.resp.response.request.path_url
+            else:
+                logging.warning("cdn_host is must started with 'https' or 'http' ")
+                cdn_url = url
+        else:
+            cdn_url = url
+        return dict(url=url, internal_url=internal_url, cdn_url=cdn_url)
+
+
 
 if __name__ == '__main__':
-    ak = 'weqeqwewq'
-    st = 'asdasdsdasdsweqw'
+    ak = '***********'
+    st = '***********'
     bk = 'fulan-test'
     alioss = Oss(ak, st, bk)
-    alioss.download_dir('tmp', 'tests')
+    # alioss.download_dir('tmp', 'tests')
+    print(alioss.push_file('a.html', 'aaa/aaaa/aaaaaaaaaaa.html'))
